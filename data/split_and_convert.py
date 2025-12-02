ea2a2c115ae8eb526129c3b51920298ea0ec2716
CHT713 <1491997289@qq.com>
2025-12-02 16:49:57 +0800

    切割数据集 转换为smiles格式

--- data/split_and_convert.py
+++ data/split_and_convert.py
@@ -3,7 +3,7 @@ from rdkit import Chem
 
 split_by_name = torch.load('split_by_name.pt')
 path_prefix = '/home/cht/DiffDec-master/data/crossdocked_pocket10/'
-f = open('train_smi1.smi', 'w')
+f = open('train_smi.smi', 'w')
 for i in range(len(split_by_name['train'])):
     ligand_filename = path_prefix + split_by_name['train'][i][1]
     try:
@@ -15,7 +15,7 @@ for i in range(len(split_by_name['train'])):
     smi = Chem.MolToSmiles(mol, isomericSmiles=False, canonical=True)
     f.write(smi + '\n')
 f.close()
-f = open('test_smi.smi1', 'w')
+f = open('test_smi.smi', 'w')
 for i in range(len(split_by_name['test'])):
     ligand_filename = path_prefix + split_by_name['test'][i][1]
     mol = next(iter(Chem.SDMolSupplier(ligand_filename, removeHs=True)))
